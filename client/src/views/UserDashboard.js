a645c0dbb6d0b77f009c978540d0c61895d8d41b
blakebonasera <60533377+blakebonasera@users.noreply.github.com>
2020-11-18 14:36:45 -0800

    formating with bootstrap

--- client/src/views/UserDashboard.js
+++ client/src/views/UserDashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Calendar from './calendar';
+import NewRepair from './NewRepair';
 
 const UserDashboard = props => {
     const [user, setUser] = useState({
@@ -26,19 +27,23 @@ const UserDashboard = props => {
     }, [])
 
     return (
-        <div>
+        <div className="container">
+            <div className="row offset-4">
             <h2>Welcome {user.firstName}</h2>
-            <div className="mt-4">
+            </div>
+            <div className="row offset-3">
                 <button className="btn btn-primary mr-5" onClick={() => request("repair") }>Request a Repair</button>
                 <button className="btn btn-primary" onClick={() => request("reserve")}>Reserve a Time</button>
             </div>
+            
             {
                 requestType === "repair" ?
-                <p className="m-2 p-5">Repair Form goes here</p>:
+                <NewRepair />:
                 requestType === "reserve" ?
                 <Calendar path="/calendar"/>:
                 ""
             }
+            
             
             <div>
                 <h3>Future chat page???</h3>
